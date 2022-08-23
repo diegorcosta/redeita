357b46d070247477d96460d1313d6b303cd3a748
Diego Costa <dgrcosta@gmail.com>
2022-08-23 09:02:23 -0300

    banner

--- src/components/Programas.js
+++ src/components/Programas.js
@@ -9,12 +9,12 @@ import "../styles/programas.scss";
 const Programas = () => {
   const slideLeft = () => {
     var slider = document.getElementById("slider");
-    slider.scrollLeft = slider.scrollLeft - 500;
+    slider.scrollLeft = slider.scrollLeft - 240;
   };
 
   const slideRight = () => {
     var slider = document.getElementById("slider");
-    slider.scrollLeft = slider.scrollLeft + 500;
+    slider.scrollLeft = slider.scrollLeft + 240;
   };
 
   return (
